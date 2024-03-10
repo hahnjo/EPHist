c8b87ff4d772ed990ca71c7dfb412e204db17be6
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-03-10 13:04:35 +0100

    Use reinterpret_cast instead of std::get

--- include/EPHist/EPHist.hxx
+++ include/EPHist/EPHist.hxx
@@ -23,13 +23,29 @@ template <typename T> class EPHist final {
 
   std::vector<AxisVariant> fAxes;
 
+  void VerifyAxesPtrs() const {
+    for (auto &axis : fAxes) {
+      if (const auto *regular = std::get_if<RegularAxis>(&axis)) {
+        assert(regular == reinterpret_cast<const RegularAxis *>(&axis));
+      } else if (const auto *variable = std::get_if<VariableBinAxis>(&axis)) {
+        assert(variable == reinterpret_cast<const VariableBinAxis *>(&axis));
+      } else {
+        assert(0 && "unknown axis type");
+      }
+    }
+  }
+
 public:
   EPHist(std::size_t numBins, double low, double high)
       : fData(new T[numBins]{}), fNumBins(numBins),
-        fAxes({RegularAxis(numBins, low, high)}) {}
+        fAxes({RegularAxis(numBins, low, high)}) {
+    VerifyAxesPtrs();
+  }
   explicit EPHist(const RegularAxis &axis)
       : fData(new T[axis.GetNumBins()]{}), fNumBins(axis.GetNumBins()),
-        fAxes({axis}) {}
+        fAxes({axis}) {
+    VerifyAxesPtrs();
+  }
   explicit EPHist(const std::vector<RegularAxis> &axes) {
     fNumBins = 1;
     for (auto &&axis : axes) {
@@ -37,10 +53,13 @@ public:
       fNumBins *= axis.GetNumBins();
     }
     fData.reset(new T[fNumBins]{});
+    VerifyAxesPtrs();
   }
   explicit EPHist(const VariableBinAxis &axis)
       : fData(new T[axis.GetNumBins()]{}), fNumBins(axis.GetNumBins()),
-        fAxes({axis}) {}
+        fAxes({axis}) {
+    VerifyAxesPtrs();
+  }
 
   EPHist(const EPHist<T> &) = delete;
   EPHist(EPHist<T> &&) = default;
@@ -66,12 +85,20 @@ public:
 private:
   template <std::size_t I, typename... A>
   std::size_t ComputeBin(std::size_t bin, const std::tuple<A...> &args) const {
-    if (const auto *regular = std::get_if<RegularAxis>(&fAxes[I])) {
+    switch (fAxes[I].index()) {
+    case 0: {
+      const auto *regular = reinterpret_cast<const RegularAxis *>(&fAxes[I]);
       bin *= regular->GetNumBins();
       bin += regular->ComputeBin(std::get<I>(args));
-    } else if (const auto *variable = std::get_if<VariableBinAxis>(&fAxes[I])) {
+      break;
+    }
+    case 1: {
+      const auto *variable =
+          reinterpret_cast<const VariableBinAxis *>(&fAxes[I]);
       bin *= variable->GetNumBins();
       bin += variable->ComputeBin(std::get<I>(args));
+      break;
+    }
     }
     if constexpr (I + 1 < sizeof...(A)) {
       return ComputeBin<I + 1>(bin, args);
@@ -83,7 +110,7 @@ private:
   std::size_t ComputeBin(std::size_t bin,
                          const typename Axis::ArgumentType &arg,
                          const typename Axes::ArgumentType &...args) const {
-    const auto &axis = std::get<Axis>(fAxes[I]);
+    const auto &axis = *reinterpret_cast<const Axis *>(&fAxes[I]);
     bin = bin * axis.GetNumBins() + axis.ComputeBin(arg);
     if constexpr (sizeof...(Axes) > 0) {
       return ComputeBin<I + 1, Axes...>(bin, args...);
