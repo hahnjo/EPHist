069ba5561425c142fce5b32b5bc685a53bf8159e
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2024-05-02 11:17:25 +0200

    Implement naive ParallelHelper and FillContext
    For now, calls to Fill are just forwarded to FillAtomic of the
    underlying histogram.

---
+++ include/EPHist/ParallelHelper.hxx
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef EPHIST_PARALLELHELPER
+#define EPHIST_PARALLELHELPER
+
+#include "EPHist.hxx"
+
+#include <memory>
+#include <mutex>
+#include <vector>
+
+namespace EPHist {
+
+template <typename T> class ParallelHelper;
+
+template <typename T> class FillContext final {
+  friend class ParallelHelper<T>;
+
+private:
+  EPHist<T> *fHist;
+
+  explicit FillContext(EPHist<T> &hist) : fHist(&hist) {}
+  FillContext(const FillContext<T> &) = delete;
+  FillContext(FillContext<T> &&) = default;
+  FillContext<T> &operator=(const FillContext<T> &) = delete;
+  FillContext<T> &operator=(FillContext<T> &&) = default;
+
+public:
+  ~FillContext() { Flush(); }
+
+  void Flush() {
+    // Nothing to do for now...
+  }
+
+  template <typename... A> void Fill(const std::tuple<A...> &args) {
+    if (sizeof...(A) != fHist->GetNumDimensions()) {
+      throw std::invalid_argument("invalid number of arguments to Fill");
+    }
+    fHist->FillAtomic(args);
+  }
+
+  template <typename... A> void Fill(const A &...args) {
+    if (sizeof...(A) != fHist->GetNumDimensions()) {
+      throw std::invalid_argument("invalid number of arguments to Fill");
+    }
+    Fill(std::forward_as_tuple(args...));
+  }
+
+  template <class... Axes>
+  void Fill(const typename Axes::ArgumentType &...args) {
+    if (sizeof...(Axes) != fHist->GetNumDimensions()) {
+      throw std::invalid_argument("invalid number of arguments to Fill");
+    }
+    fHist->template FillAtomic<Axes...>(args...);
+  }
+};
+
+template <typename T> class ParallelHelper final {
+private:
+  std::shared_ptr<EPHist<T>> fHist;
+
+  std::mutex fMutex;
+  std::vector<std::weak_ptr<FillContext<T>>> fFillContexts;
+
+public:
+  explicit ParallelHelper(std::shared_ptr<EPHist<T>> hist)
+      : fHist(std::move(hist)) {}
+  ParallelHelper(const ParallelHelper<T> &) = delete;
+  ParallelHelper(ParallelHelper<T> &&) = delete;
+  ParallelHelper<T> &operator=(const ParallelHelper<T> &) = delete;
+  ParallelHelper<T> &operator=(ParallelHelper<T> &&) = delete;
+  ~ParallelHelper() {
+    for (const auto &context : fFillContexts) {
+      assert(context.expired());
+    }
+    Flush();
+  }
+
+  void Flush() {
+    // Nothing to do for now...
+  }
+
+  std::shared_ptr<FillContext<T>> CreateFillContext() {
+    std::lock_guard g(fMutex);
+
+    // Cannot use std::make_shared because the constructor of FillContext is
+    // private. Also it would mean that the (direct) memory of all contexts
+    // stays around until the vector of weak_ptr's is cleared.
+    std::shared_ptr<FillContext<T>> context(new FillContext<T>(*fHist));
+    fFillContexts.push_back(context);
+    return context;
+  }
+};
+
+} // namespace EPHist
+
+#endif
---
+++ test/parallel.cxx
@@ -0,0 +1,313 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#include <EPHist/EPHist.hxx>
+#include <EPHist/ParallelHelper.hxx>
+#include <EPHist/RegularAxis.hxx>
+
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <tuple>
+
+TEST(ParallelHelperIntRegular1D, CreateFillContext) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<int>>(Bins, 0, Bins);
+  EPHist::ParallelHelper helper(h1);
+
+  auto context1 = helper.CreateFillContext();
+  auto context2 = helper.CreateFillContext();
+}
+
+TEST(ParallelHelperIntRegular1D, Fill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<int>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(i);
+    }
+    context->Fill(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperIntRegular1D, FillTuple) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<int>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(std::make_tuple(-100));
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(std::make_tuple(i));
+    }
+    context->Fill(std::make_tuple(100));
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperIntRegular1D, TemplatedFill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<int>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill<EPHist::RegularAxis>(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill<EPHist::RegularAxis>(i);
+    }
+    context->Fill<EPHist::RegularAxis>(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperLongLongRegular1D, Fill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<long long>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(i);
+    }
+    context->Fill(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperLongLongRegular1D, FillTuple) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<long long>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(std::make_tuple(-100));
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(std::make_tuple(i));
+    }
+    context->Fill(std::make_tuple(100));
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperLongLongRegular1D, TemplatedFill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<long long>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill<EPHist::RegularAxis>(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill<EPHist::RegularAxis>(i);
+    }
+    context->Fill<EPHist::RegularAxis>(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperFloatRegular1D, Fill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<float>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(i);
+    }
+    context->Fill(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_FLOAT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperFloatRegular1D, FillTuple) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<float>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(std::make_tuple(-100));
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(std::make_tuple(i));
+    }
+    context->Fill(std::make_tuple(100));
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_FLOAT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperFloatRegular1D, TemplatedFill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<float>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill<EPHist::RegularAxis>(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill<EPHist::RegularAxis>(i);
+    }
+    context->Fill<EPHist::RegularAxis>(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_FLOAT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperDoubleRegular1D, Fill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<double>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(i);
+    }
+    context->Fill(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_FLOAT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperDoubleRegular1D, FillTuple) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<double>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(std::make_tuple(-100));
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(std::make_tuple(i));
+    }
+    context->Fill(std::make_tuple(100));
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_FLOAT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperDoubleRegular1D, TemplatedFill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<double>>(Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill<EPHist::RegularAxis>(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill<EPHist::RegularAxis>(i);
+    }
+    context->Fill<EPHist::RegularAxis>(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    EXPECT_FLOAT_EQ(h1->GetBinContent(i), 1);
+  }
+}
+
+TEST(ParallelHelperDoubleBinWithErrorRegular1D, Fill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<EPHist::DoubleBinWithError>>(
+      Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(i);
+    }
+    context->Fill(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    auto &binWithError = h1->GetBinContent(i);
+    EXPECT_FLOAT_EQ(binWithError.fSum, 1);
+    EXPECT_FLOAT_EQ(binWithError.fSum2, 1);
+  }
+}
+
+TEST(ParallelHelperDoubleBinWithErrorRegular1D, FillTuple) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<EPHist::DoubleBinWithError>>(
+      Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill(std::make_tuple(-100));
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill(std::make_tuple(i));
+    }
+    context->Fill(std::make_tuple(100));
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    auto &binWithError = h1->GetBinContent(i);
+    EXPECT_FLOAT_EQ(binWithError.fSum, 1);
+    EXPECT_FLOAT_EQ(binWithError.fSum2, 1);
+  }
+}
+
+TEST(ParallelHelperDoubleBinWithErrorRegular1D, TemplatedFill) {
+  static constexpr std::size_t Bins = 20;
+  auto h1 = std::make_shared<EPHist::EPHist<EPHist::DoubleBinWithError>>(
+      Bins, 0, Bins);
+
+  {
+    EPHist::ParallelHelper helper(h1);
+    auto context = helper.CreateFillContext();
+    context->Fill<EPHist::RegularAxis>(-100);
+    for (std::size_t i = 0; i < Bins; i++) {
+      context->Fill<EPHist::RegularAxis>(i);
+    }
+    context->Fill<EPHist::RegularAxis>(100);
+  }
+
+  for (std::size_t i = 0; i < h1->GetTotalNumBins(); i++) {
+    auto &binWithError = h1->GetBinContent(i);
+    EXPECT_FLOAT_EQ(binWithError.fSum, 1);
+    EXPECT_FLOAT_EQ(binWithError.fSum2, 1);
+  }
+}
